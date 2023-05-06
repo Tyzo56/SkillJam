dd8c3a9618389d36e3bc579d20d1fd7d502cdf7f
Arkkul <104141207+Arkkul@users.noreply.github.com>
2023-05-06 15:45:36 +0300

    0.8
    следящая за игроком камера

---
+++ Livsi/Assets/Scripts/bulletDelete.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletDelete : MonoBehaviour
+{
+    float speed = 4f;
+    float TimeToDisable = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(setDisable());
+    }
+
+    // Update is called once per frame
+   
+    IEnumerator setDisable()
+    {
+        yield return new WaitForSeconds(TimeToDisable);
+        gameObject.SetActive(false);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        StopCoroutine(setDisable());
+        gameObject.SetActive(false);
+    }
+}
